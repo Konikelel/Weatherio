567e029482e3701cf905b012b542b944655cc8a1
Konikelel <wiktor.albinowski@makit.pl>
2024-07-13 13:48:58 +0200

    create module from models on backend

---
+++ python/models/__init__.py
@@ -0,0 +1,6 @@
+from .air_pollution import AirPollution  # type: ignore
+from .location import Location  # type: ignore
+from .weather import WeatherCurrent, WeatherForecast  # type: ignore
+from .weather_data import WeatherData  # type: ignore
+from .weather_desc import WeatherDesc  # type: ignore
+from .wind import Wind  # type: ignore
