64e7fb978ff650ee1c6f42663d4e7bb40c68a00f
Konikelel <wiktor.albinowski@makit.pl>
2024-07-13 13:49:40 +0200

    build fastapi on backend

---
+++ python/main.py
@@ -0,0 +1,57 @@
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+
+from typing import Annotated, Literal
+
+import uvicorn
+from api_handler import fetchAirPollution, fetchCurrentWeather, fetchForecast
+from fastapi import FastAPI, HTTPException, Query
+
+app = FastAPI()
+
+
+@app.get("/")
+async def test():
+    return {"message": "Hello World"}
+
+
+@app.get("/weather/current")
+async def current_weather(
+    city: Annotated[str, Query(description="Name of the city to get weather data", min_length=1, max_length=30)]
+):
+    weather = await fetchCurrentWeather(city=city)
+
+    if not weather:
+        raise HTTPException(status_code=400, detail="Could not fetch weather data from API")
+
+    return weather
+
+
+@app.get("/weather/forecast")
+async def forecast_weather(
+    city: Annotated[str, Query(description="Name of the city to get weather data", min_length=1, max_length=30)],
+    interval: Annotated[Literal["days", "hours"], Query(description="Interval of forecast data")],
+):
+    weather = await fetchForecast(city=city, interval=interval)
+
+    if not weather:
+        raise HTTPException(status_code=400, detail="Could not fetch weather data from API")
+
+    return weather
+
+
+@app.get("/weather/pollution")
+async def pollution_weather(
+    city: Annotated[str, Query(description="Name of the city to get weather data", min_length=1, max_length=30)]
+):
+    weather = await fetchAirPollution(city=city)
+
+    if not weather:
+        raise HTTPException(status_code=400, detail="Could not fetch weather data from API")
+
+    return weather
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
