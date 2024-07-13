43ce1475afc6dfb0b05274c458479fa1275d68bc
Konikelel <wiktor.albinowski@makit.pl>
2024-07-13 15:16:25 +0200

    add navbar component to app, change app styles and and minimalistic style to navbar component

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,20 +1,11 @@
 import { Component } from '@angular/core';
-import { FiveDaysForecastComponent } from './five-days-forecast/five-days-forecast.component';
-import { OneDayForecastComponent } from './one-day-forecast/one-day-forecast.component';
-import { TodaysHighlightsComponent } from './todays-highlights/todays-highlights.component';
-import { WeatherNowComponent } from './weather-now/weather-now.component';
-
+import { NavbarComponent } from './navbar/navbar.component';
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [
-        FiveDaysForecastComponent,
-        OneDayForecastComponent,
-        TodaysHighlightsComponent,
-        WeatherNowComponent,
-    ],
+    imports: [NavbarComponent],
 })
 export class AppComponent {
     title = 'weatherio';
