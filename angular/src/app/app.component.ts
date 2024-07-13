5d6008e3e242088be8faa6ec5db37c57098fddd2
Konikelel <wiktor.albinowski@makit.pl>
2024-07-13 17:23:33 +0200

    add todays-highlights component

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
+import { TodaysHighlightsComponent } from './todays-highlights/todays-highlights.component';
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [NavbarComponent],
+    imports: [NavbarComponent, TodaysHighlightsComponent],
 })
 export class AppComponent {
     title = 'weatherio';
---
+++ angular/src/app/todays-highlights/todays-highlights.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-todays-highlights',
+    standalone: true,
+    imports: [],
+    templateUrl: './todays-highlights.component.html',
+    styleUrl: './todays-highlights.component.scss',
+})
+export class TodaysHighlightsComponent {}
