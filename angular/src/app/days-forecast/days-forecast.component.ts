f2fc80b9d934d551494ee201817d658449fbf2cf
Konikelel <wiktoralbinowski@gmail.com>
2024-07-14 13:40:10 +0200

    create days-forecast add global styles for text and apply it to app

---
+++ angular/src/app/days-forecast/days-forecast.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-days-forecast',
+  standalone: true,
+  imports: [],
+  templateUrl: './days-forecast.component.html',
+  styleUrl: './days-forecast.component.css'
+})
+export class DaysForecastComponent {
+
+}
